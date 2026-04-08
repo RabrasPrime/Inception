09716a365c4a74a66d6374a00865e078c65a7f29
tjooris <tjooris@tjooris.42.fr>
2026-04-08 17:57:05 +0200

    mariadb encore

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -1,4 +1,35 @@
 #!/bin/sh
-set -e
-echo "Initialisation personnalisée"
-exec mysqld
+
+if [ ! -d "/run/mysqld" ]; then
+    mkdir -p /run/mysqld
+    chown -R mysql:mysql /run/mysqld
+fi
+
+ARGS="--user=mysql --console --bind-address=0.0.0.0"
+
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+
+    mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+
+    cat << EOF > /tmp/init_db.sql
+USE mysql;
+FLUSH PRIVILEGES;
+
+DELETE FROM mysql.user WHERE User='';
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test';
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';
+CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE}\`;
+CREATE USER '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE}\`.* TO '${MARIADB_USER}'@'%';
+
+FLUSH PRIVILEGES;
+EOF
+
+    exec mysqld --user=mysql --init-file=/tmp/init_db.sql
+
+    ARGS="$ARGS --init-file=/tmp/init_db.sql"
+fi
+
+exec mysqld $ARGS
