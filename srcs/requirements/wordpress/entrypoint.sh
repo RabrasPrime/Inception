86d5a74e2ee85b4dcc6c1ee52750b66fac7d2f0e
tjooris <tjooris@tjooris.42.fr>
2026-04-13 15:05:37 +0200

    add environement + finish dockerfile wordpress

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -1,1 +1,9 @@
 #!/bin/sh
+
+if [ ! -f /var/www/html/wp-config.php ]; then
+    wp core download --allow-root
+    wp config create --allow-root --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST
+    wp core install --allow-root --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_PASS --admin_email=$WP_EMAIL
+fi
+
+exec php-fpm83 -F
