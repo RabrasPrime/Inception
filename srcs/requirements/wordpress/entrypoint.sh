1b4a6a5d9b60748d8447aef7acda5543514b1e2c
tjooris <tjooris@tjooris.42.fr>
2026-04-13 17:13:53 +0200

    finish the entrypoint.sh

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -1,9 +1,25 @@
 #!/bin/sh
 
+DB_PASS=$(cat /run/secrets/db-password)
+WP_ADMIN_PASS=$(cat /run/secrets/wp-admin-password)
+
+until mysqladmin ping -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASS" --silent 2>/dev/null; do
+    sleep 2
+done
+
 if [ ! -f /var/www/html/wp-config.php ]; then
-    wp core download --allow-root
-    wp config create --allow-root --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST
-    wp core install --allow-root --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_PASS --admin_email=$WP_EMAIL
+    wp config create --allow-root \
+        --dbname="$DB_NAME" \
+        --dbuser="$DB_USER" \
+        --dbpass="$DB_PASS" \
+        --dbhost="$DB_HOST"
+
+    wp core install --allow-root \
+        --url="$WP_URL" \
+        --title="$WP_TITLE" \
+        --admin_user="$WP_ADMIN" \
+        --admin_password="$WP_ADMIN_PASS" \
+        --admin_email="$WP_EMAIL"
 fi
 
 exec php-fpm83 -F
