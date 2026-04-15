0f88405ec9181d3fc30f6e82da643aef0491063b
tjooris <tjooris@tjooris.42.fr>
2026-04-15 14:02:54 +0200

    add nginx with conf and entrypoint

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+DB_PASSWORD=$(cat /run/secrets/db-password)
+DB_ROOT_PASSWORD=$(cat /run/secrets/db-root-password)
+
 if [ ! -d "/run/mysqld" ]; then
     mkdir -p /run/mysqld
     chown -R mysql:mysql /run/mysqld
@@ -19,10 +22,10 @@ DELETE FROM mysql.user WHERE User='';
 DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test';
 
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';
-CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE}\`;
-CREATE USER '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';
-GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE}\`.* TO '${MARIADB_USER}'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
+CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
+CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
 
 FLUSH PRIVILEGES;
 EOF
--- srcs/requirements/nginx/entrypoint.sh
+++ srcs/requirements/nginx/entrypoint.sh
@@ -1,1 +1,9 @@
 #!/bin/sh
+
+#!/bin/sh
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout /etc/ssl/private/nginx.key \
+    -out /etc/ssl/certs/nginx.crt \
+    -subj "/CN=tjooris.42.fr"
+
+nginx -g 'daemon off;'
