516997c228399d14a3a612d8df326c3ef4c22fb4
tjooris <tjooris@tjooris.fr>
2026-05-04 15:47:00 +0200

    all bonus done

--- srcs/requirements/bonus/vsftpd/entrypoint.sh
+++ srcs/requirements/bonus/vsftpd/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
+#
 set -e
-
 FTP_PASSWORD=$(cat /run/secrets/ftp-password | tr -d '\n')
 
 mkdir -p /var/run/vsftpd/empty
@@ -8,16 +8,12 @@ chown root:root /var/run/vsftpd/empty
 chmod 755 /var/run/vsftpd/empty
 
 if ! id "$FTP_USER" >/dev/null 2>&1; then
-  adduser -D -h /home/"$FTP_USER" -s /bin/sh "$FTP_USER"
-  echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
+    adduser -D -h /home/"$FTP_USER" -s /bin/sh "$FTP_USER"
+    echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
 fi
 
-mkdir -p /var/www/localhost/htdocs
-chown -R "$FTP_USER":"$FTP_USER" /var/www/localhost/htdocs
-chmod -R 755 /var/www/localhost/htdocs
-
+chown -R "$FTP_USER":"$FTP_USER" /var/www/html
+chmod -R 755 /var/www/html
 mkdir -p /var/log/vsftpd
 touch /var/log/vsftpd.log
-chown "$FTP_USER":"$FTP_USER" /var/log/vsftpd.log
-
 exec vsftpd /etc/vsftpd/vsftpd.conf
