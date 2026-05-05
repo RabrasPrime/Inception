52a234e91e606b2b4fc57a146e5484ed209c8d44
tjooris <tjooris@tjooris.fr>
2026-05-05 12:31:34 +0200

    patching

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -20,6 +20,11 @@ if [ ! -f /var/www/html/wp-config.php ]; then
         --admin_user="$WP_ADMIN" \
         --admin_password="$WP_ADMIN_PASS" \
         --admin_email="$WP_EMAIL"
+
+    wp config set WP_REDIS_HOST "$WP_REDIS_HOST" --allow-root
+    wp config set WP_REDIS_PORT "$WP_REDIS_PORT" --allow-root
+    wp plugin install redis-cache --activate --allow-root
+    wp redis enable --allow-root
 fi
 
 sed -i 's/listen = 127.0.0.1:9000/listen = 0.0.0.0:9000/' /etc/php83/php-fpm.d/www.conf
